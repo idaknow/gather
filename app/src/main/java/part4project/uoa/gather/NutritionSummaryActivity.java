c8259613fb3b29819ed1619791458a378ffb49a8
Ida <ides542@aucklanduni.ac.nz>
2017-07-29 21:13:42 +1200

    added two other summary activities

---
+++ app/src/main/java/part4project/uoa/gather/NutritionSummaryActivity.java
@@ -0,0 +1,20 @@
+package part4project.uoa.gather;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NutritionSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_summary);
+    }
+
+    public void intentMainActivity(View view) {
+        startActivity(new Intent(this,MainActivity.class));
+    }
+
+}
